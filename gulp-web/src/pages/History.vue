9a8ac63081aa85c0027e9d6a3164667c08d5503e
hellozo0 <ahju0011@gmail.com>
2025-04-07 16:34:00 +0900

    feat : 헤더 Router 페이지 생성

---
+++ gulp-web/src/pages/History.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card card-body">
+    <h2>History</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'History',
+};
+</script>
---
+++ gulp-web/src/pages/SignUp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card card-body">
+    <h2>SignUp</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+};
+</script>
