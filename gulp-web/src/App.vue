fa699941618f780d0415afe13dbaf49bfcfd829b
hellozo0 <ahju0011@gmail.com>
2025-04-07 22:36:24 +0900

    feat : Header 연결 부분 160px 세팅
    - 안쓰는 css 파일들 삭제

--- gulp-web/src/App.vue
+++ gulp-web/src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import Header from './components/Header.vue';
 </script>
 
 <template>
   <div class="container">
     <Header />
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/mypage">Mypage</RouterLink>
-    </nav>
-    <router-view />
+    <main class="page-content">
+      <RouterView />
+    </main>
   </div>
 </template>
+
+<style scoped>
+.page-content {
+  padding-top: 160px; /* banner + navbar 높이만큼 조절 (대략 140~150px) */
+}
+</style>
--- gulp-web/src/main.js
+++ gulp-web/src/main.js
@@ -1,12 +1,9 @@
-import './assets/main.css';
-import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { createPinia } from 'pinia';
+
 const app = createApp(App);
-const pinia = createPinia();
 
 app.use(router);
-app.use(pinia);
+
 app.mount('#app');
