7b3cdd92eaa33ccfdfb889746e1b1b82f398d63e
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-04-09 17:50:42 +0900

    calender 수정

--- gulp-web/src/main.js
+++ gulp-web/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import VCalendar from 'v-calendar';
 import 'v-calendar/style.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const app = createApp(App);
 const pinia = createPinia();
