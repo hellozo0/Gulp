176ec6f9330901ff46fa135dd532145c59c5c522
Dayeon <chiikawa143@gmail.com>
2025-04-09 11:38:03 +0900

    calendar layout 추가

--- gulp-web/src/main.js
+++ gulp-web/src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(VCalendar, {});
 
 app.mount('#app');
